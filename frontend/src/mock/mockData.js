c507b32bc9f7c71552c0f4844b56a6caa99a17f0
Federico Sidlik <fedesidlik@gmail.com>
2026-02-15 17:03:14 -0300

    Actualización rápida

--- frontend/src/mock/mockData.js
+++ frontend/src/mock/mockData.js
@@ -143,7 +143,23 @@ export const socialLinks = {
 };
 
 export const lyrics = [
-  { id: 1, title: "AVE DE FUEGO", lyrics: `[Letra próximamente]` },
+  { id: 1, title: "AVE DE FUEGO", lyrics: "Surgen en el bosque cuando todos menos lo esperan,\n\
+seres tenebrosos, entre las tinieblas\n\
+Se acercan despacio mientras ven como te rodean,\n\
+Fueron invocados por una hechicera\n\n\
+Y cuando todo el mundo piense que ya te vencieron,\n\
+o cuando todo el mundo apueste que vas a perder\n\
+Resurgirás como un ave de fuego\n\
+enfrentando al mundo una y otra vez\n\
+Oh  oh-oh-oh …oh  oh-oh-oh yeah!\n\n\
+Causan la desolación donde ya nada queda\n\
+Como un exterminador pisando calaveras\n\
+Se terminan las opciones. Ya es tiempo de que retrocedan\n\
+Naciste para enfrentarlos. Y esto dice tu leyenda\n\n\
+Cuando todo el mundo piense que ya te vencieron,\n\
+o cuando todo el mundo apueste que vas a perder\n\
+Resurgirás como un ave de fuego\n\
+enfrentando al mundo una y otra vez"},
   { id: 2, title: "BRILLANDO EN LA OSCURIDAD", lyrics: "Buscando el santuario donde poder descansar\n\
 Caminando a paso lento me alejé de la ciudad\n\
 Muchas cruces puedo ver, frías tumbas que no brillan desde ayer\n\
@@ -155,7 +171,7 @@ y al señalar mi propia tumba desencadenó la reacción\n\
 La ciudad va quedando atrás y sus luces van dejando de brillar\n\
 Dime nena donde estás y con mi luz voy a encontrarte en la oscuridad\n\n\
 Brillando en la oscuridad, yo te voy a encontrar\n\
-Brillando en la oscuridad" },
+Brillando en la oscuridad"},
   { id: 3, title: "CAZARRECOMPENSAS", lyrics: "Un pistolero cabalgando en su corcel,\n\
 vino al pueblo, al atardecer\n\
 Y cuando en el bar entró, la muerte entró también\n\
@@ -175,7 +191,7 @@ Cazarrecompensa fuera de la ley\n\
 Cazarrecompensas por siempre seré\n\n\
 Y cuando del bar se fue, la muerte partió también\n\
 Su leyenda hablaba por él\n\
-Su leyenda, hablaba por él" },
+Su leyenda, hablaba por él"},
   { id: 4, title: "CÓMO LLEGAR A VOS", lyrics: "Gritos desesperados de ultratumba,\n\
 guiados por ilusiones de hoy y también de ayer,\n\
 hacen que al avanzar entre las penumbras,\n\
@@ -198,18 +214,220 @@ Perdido estoy en una ilusión.\n\
 Perdido estoy y no sé cómo llegar a vos\n\n\
 Cómo llegar a vos Oh-oh-oh-oh\n\
 Cómo llegar a vos Oh-oh-oh-oh" },
-  { id: 5, title: "DAME UNA RAZÓN", lyrics: `[Letra próximamente]` },
-  { id: 6, title: "DIOSES DEL OLIMPO", lyrics: `[Letra próximamente]` },
-  { id: 7, title: "DISCRIMINACIÓN", lyrics: `[Letra próximamente]` },
-  { id: 8, title: "EL JUEGO", lyrics: `[Letra próximamente]` },
-  { id: 9, title: "EL MAGO", lyrics: `[Letra próximamente]` },
-  { id: 10, title: "EL SECRET0", lyrics: `[Letra próximamente]` },
-  { id: 11, title: "EN LAS SOMBRAS DE LA NOCHE", lyrics: `[Letra próximamente]` },
-  { id: 12, title: "ESPECTRO FANTASMAL", lyrics: `[Letra próximamente]` },
-  { id: 13, title: "ESPEJISMOS", lyrics: `[Letra próximamente]` },
-  { id: 14, title: "GAVILÁN", lyrics: `[Letra próximamente]` },
-  { id: 15, title: "GUARDIANES DE LA NIEBLA", lyrics: `[Letra próximamente]` },
-  { id: 16, title: "HASTA EL FINAL", lyrics: `[Letra próximamente]` },
+  { id: 5, title: "DAME UNA RAZÓN", lyrics: "Dejá libre al guerrero que habita tu ser interior\n\
+y gritá a los cuatro vientos, gritá con todo el furor\n\
+El pasado ya es pasado y el futuro no llegó\n\
+Disfruta el día presente, disfruta el día de hoy\n\n\
+Dame una razón para volver\n\
+Dame una razón para creer\n\
+Dame una razón para seguir\n\
+Dame una razón para existir\n\
+Para olvidar. Para soñar\n\
+Para no caer. Para vencer\n\
+Para olvidar. Para soñar\n\
+Para no caer. Para vencer\n\n\
+Dejá libre al guerrero que habita tu interior\n\
+gritá a los cuatro vientos, con todo el furor\n\
+Para olvidar. Para soñar\n\
+Para no caer. Para vencer\n\
+Para olvidar. Para soñar\n\
+Para no caer. Para vencer\n\n\
+Dame una razón. Oh-oh-oh-oh\n\
+Dame una razón. Oh-oh-oh-oh"},
+  { id: 6, title: "DIOSES DEL OLIMPO", lyrics: "Si pueden invocar a los Dioses del Olimpo,\n\
+aunque pasen mil años no los van a olvidar\n\
+Prepárense a escuchar, y sean bienvenidos\n\
+Oh cuántas historias les voy a contar...\n\n\
+Los Dioses del olimpo te dieron la misión,\n\
+y para realizarla serás un gladiador\n\
+Frente a la injusticia se comienzan a unir\n\
+Y los que te siguieron, aprenden a rugir\n\n\
+Siempre habrá gente revolucionaria,\n\
+quejándose por todo, pero no haciendo nada\n\
+Y las flechas que cubren, todo el cielo gris,\n\
+solo te hacen más fuerte, para combatir\n\n\
+…Oh oh oh Dioses del Olimpo\n\
+Aunque pasen mil años no los van a olvidar\n\
+Oh oh oh sean bienvenidos\n\
+Oh cuántas historias les voy a contar..."},
+  { id: 7, title: "DISCRIMINACIÓN", lyrics: "Puedes percibir una sensación,\n\
+que está en el aire, a tu alrededor\n\
+No te importará, lo que piensen de vos,\n\
+porque a tus amigos, no les importó\n\n\
+No te importará, lo que piensen de vos, \n\
+porque a tus amigos, nunca les importó\n\n\
+Podrás compartir alegría y dolor,\n\
+toda tu locura, con tu otro yo\n\
+El mismo sentir, la misma canción\n\
+Cosas de la vida, toda tu visión\n\
+Pues no saben de discriminación\n\
+No a la discriminación\n\
+No saben de discriminación\n\
+No a la discriminación (yeah)\n\n\
+No te importará, lo que piensen de vos,\n\
+porque a tus amigos, nunca les importó\n\
+No te importará, lo que piensen de vos, \n\
+porque a tus amigos, nunca les importó\n\
+No a la discriminación"},
+  { id: 8, title: "EL JUEGO", lyrics: "Mucho antes de nacer,\n\
+un juego misterioso ha sido planeado\n\
+Nada fácil, yo lo se,\n\
+aunque nada es imposible podría jurarlo\n\n\
+Estás en un juego,\n\
+juego milenario\n\
+y a pesar de todo\n\
+vamos a ganarlo..."},
+  { id: 9, title: "EL MAGO", lyrics: "En la era medieval una guerra estalló\n\
+Te armarás con una espada, un escudo y gran valor\n\
+Una tierra sin ley, el mago nos reveló\n\
+Y a los que atacaron, en piedra los convirtió\n\
+Revolución! Somos la revolución!\n\n\
+Cuentan que una vez en un bosque sin sol,\n\
+un castillo sin rey hechizado quedó\n\
+Revolución! Somos la revolución!\n\
+Revolución! Somos la revolución!\n\n\
+Gigantes puedes ver, guiados por un dragón\n\
+Prepárate a defender, (porque) el portal (ya) se derrumbó\n\
+Revolución! Somos la revolución!\n\
+Revolución! Somos la revolución!\n\n\
+Una tierra sin ley, el mago nos reveló\n\
+Y a los que atacaron, en piedra los convirtió\n\
+Revolución! Somos la revolución!\n\
+Revolución! Somos la revolución!"},
+  { id: 10, title: "EL SECRET0", lyrics: "La noche nos acompaña, y va creciendo la emoción\n\
+Hasta el fin de los tiempos, estaremos siempre con vos...\n\n\
+Érase una vez cuando sucedió\n\
+Ya nada fue igual. El show comenzó!\n\
+Es el secreto entre vos y yo\n\
+Jamás dejes que se muera la ilusión\n\n\
+Podrás recordar todo lo que fue,\n\
+y nunca olvidarás cuando te encontré\n\
+Es el secreto entre vos y yo\n\
+Jamás dejes que se muera la ilusión\n\n\
+La noche nos acompaña, y va creciendo la emoción\n\
+Hasta el fin de los tiempos, estaremos siempre con vos...\n\
+Es el secreto entre vos y yo\n\
+Jamás dejes que se muera la ilusión"},
+  { id: 11, title: "EN LAS SOMBRAS DE LA NOCHE", lyrics: "Mil espectros vi renacer, en las tumbas de la maldad\n\
+Subestimaron mi poder, y los tuve que enfrentar\n\
+Cuando menos lo imaginen, o cuando menos lo pensés,\n\
+en las sombras de la noche, necesitarás creer\n\
+Y cuando vos me necesites, o cuando vos me invoques,\n\
+en las sombras de la noche, a tu lado yo estaré\n\n\
+Disparé mi rayo de luz, contra los espectros del mal\n\
+Vi como algunos se iban, y a otros tuve que aniquilar\n\
+Verás alucinaciones, que serán tu realidad,\n\
+y en las sombras de la noche, me vas a necesitar\n\
+Y cuando vos me necesites, o cuando vos me invoques,\n\
+en las sombras de la noche, a tu lado yo estaré\n\n\
+Cuando menos lo imaginen, o cuando menos lo pensés,\n\
+en las sombras de la noche, necesitarás creer\n\
+Y cuando vos me necesites, o cuando vos me invoques,\n\
+en las sombras de la noche, a tu lado yo estaré\n\
+En las sombras de la noche, a tu lado yo estaré"},
+  { id: 12, title: "ESPECTRO FANTASMAL", lyrics: "Dime qué pensás ahora cuando estás en soledad,\n\
+cuando la ciudad se duerme y los ángeles se van\n\
+Dime qué pensas ahora cuando ya no hay que pensar,\n\
+cuando no encontrás consuelo y todo es oscuridad\n\n\
+Vagando donde no hay estrellas\n\
+Buscando alguien a quien amar\n\
+Brillando hasta que desaparezca,\n\
+avanza un espectro fantasmal\n\n\
+Pasaste el tiempo alejado concentrado en tus deseos,\n\
+dejando todo en las manos del gran dios del universo\n\
+Pasaste el tiempo encerrado en una cripta de acero,\n\
+y al salir de las cavernas quisiste volver al sueño\n\n\
+Vagando donde no hay estrellas\n\
+Buscando alguien a quien amar\n\
+Brillando hasta que desaparezca,\n\
+avanza un espectro fantasmal\n\n\
+Dime que pensas ahora cuando tu alma no da más\n\
+cuando quedaste sin fuerzas y tenes que seguir igual\n\n\
+Vagando donde no hay estrellas\n\
+Buscando alguien a quien amar\n\
+Brillando hasta que desaparezca,\n\
+avanza un espectro fantasmal"},
+  { id: 13, title: "ESPEJISMOS", lyrics: "Por ver lo que no es, te van a hipnotizar\n\
+con una simple mirada te pueden engatusar\n\
+Siempre comienza igual con juegos de seducción\n\
+donde nada es real y ya no hay percepción\n\n\
+Los espejismos solo te dejan ver\n\
+las ilusiones en las que querés creer\n\
+Los espejismos ya se desvanecerán\n\
+dentro del limbo que oculta tu realidad\n\n\
+Solo y desamparado en el destierro no logras ver\n\
+que la impaciencia puede traicionarte ya lo vas a entender\n\
+Y cuando vos te des cuenta de lo elemental\n\
+lo que pudo haber sido, ya no podrá ser jamás\n\n\
+Los espejismos solo te dejan ver\n\
+las ilusiones en las que querés creer\n\
+Los espejismos ya se desvanecerán\n\
+dentro del limbo que oculta tu realidad"},
+  { id: 14, title: "GAVILÁN", lyrics: "En un lejano pantano vigila el gavilán\n\
+Dicen que no es de este mundo. ¿Quién lo puede asegurar?\n\
+Y cuando vayan a buscarlo, no lo podrán encontrar\n\
+Solo cambiará su rumbo y se desvanecerá\n\n\
+Majestuoso gavilán, como el viento tu serás,\n\
+y atraparte aunque lo intenten no podrán\n\
+Majestuoso gavilán\n\n\
+Le pondrán miles de trampas, pero no lo engañarán\n\
+El será fiel a sí mismo, aunque lo traicionarán\n\
+No podrán hacerle daño. Libre será hasta el fin\n\
+Y antes de ser atrapado él preferirá morir\n\n\
+Majestuoso gavilán, como el viento tu serás\n\
+y atraparte aunque lo intenten no podrán\n\
+Majestuoso gavilán.\n\n\
+Criatura de la noche lo han obligado a ser\n\
+Siempre vivirá escapando. ¡Quién sabe si él va a volver!\n\
+Y cuando vayan a buscarlo, no lo podrán encontrar\n\
+Solo cambiará su rumbo y se desvanecerá\n\n\
+Majestuoso gavilán, como el viento tu serás,\n\
+y atraparte aunque lo intenten no podrán\n\
+Majestuoso gavilán"},
+  { id: 15, title: "GUARDIANES DE LA NIEBLA", lyrics: "Dibujando sombras al anochecer\n\
+Todo lo que aprendiste de un sabio sensei\n\
+Abriendo portales a tu alrededor,\n\
+liberas tu mente y ya no existe el temor\n\n\
+El misterio cuenta la leyenda,\n\
+que algunos de ustedes podrán ver\n\
+Somos los guardianes de la niebla\n\
+y viajamos en un astrotrén\n\n\
+La niebla nos cubre en la oscuridad\n\
+Se cumple la profecía y vos la vas a contar\n\
+Buscale la vuelta, para no abandonar\n\
+Y aunque parezca difícil, vos, volverás a brillar\n\n\
+El misterio cuenta la leyenda\n\
+que algunos de ustedes podrán ver\n\
+Somos los guardianes de la niebla\n\
+y viajamos en un astrotrén.\n\n\
+Guardianes de la niebla vendrán\n\
+Lo predijo un anciano sensei\n\
+Guerreros en la oscuridad,\n\
+algunos de ustedes podrán ser"},
+  { id: 16, title: "HASTA EL FINAL", lyrics: "Vas viajando por el universo\n\
+recorriendo estrellas sin brillar\n\
+Nada te parece conocido\n\
+y te fuiste sin rumbo sin mirar atrás\n\n\
+Yo no sé si hoy te voy a encontrar\n\
+Yo no sé si estas en algún lugar\n\
+Solo sé que hoy quiero un día más\n\
+Quédate conmigo hasta el final\n\n\
+Cuando pasa algo que no entiendes\n\
+dicen que por algo ha de pasar\n\
+Pero nunca cierra en tu mente\n\
+Misterios que siempre estarán\n\n\
+Yo no sé si hoy te voy a encontrar\n\
+Yo no sé si estas en algún lugar\n\
+Solo sé que hoy quiero un día más\n\
+Quédate conmigo hasta el final\n\n\
+El día que menos tu lo pienses\n\
+tal vez ya no sea un día mas\n\
+Deja que tu alma se disperse\n\
+y que se haga el viento en el mar\n\n\
+Yo no sé si hoy te voy a encontrar\n\
+Yo no sé si estas en algún lugar\n\
+Solo sé que hoy quiero un día más\n\
+Quédate conmigo hasta el final"},
   { id: 17, title: "HOY", lyrics: "En la niebla te iluminaste\n\
 ahuyentando a todos los cuervos.\n\
 Imposible será olvidarme, si (ya) soy parte de tus recuerdos\n\n\
@@ -260,11 +478,79 @@ y cuantos han abandonado desde la partida\n\n\
 Yo se que es un juego difícil que algunos pensarán abandonar\n\
 Pero a pesar de todo no te rendirás\n\n\
 Y no nos vencerán, no nos vencerán\n\
-No nos vencerán, no nos vencerán" },
-  { id: 20, title: "RENEGADO", lyrics: `[Letra próximamente]` },
-  { id: 21, title: "SALVAJE CORCEL", lyrics: `[Letra próximamente]` },
-  { id: 22, title: "SOS COMO SOS", lyrics: `[Letra próximamente]` },
-  { id: 23, title: "TRES DESEOS", lyrics: `[Letra próximamente]` },
+No nos vencerán, no nos vencerán"},
+  { id: 20, title: "RENEGADO", lyrics: "Estoy refugiado en el subsuelo\n\
+Acorralado me tienen los perros\n\
+Ya no hay salvación, me están por alcanzar\n\
+y si en verdad me quieren que me vengan a buscar.\n\
+Que me vengan a buscar...\n\
+Si en verdad me quieren, que me vengan a buscar\n\n\
+Salieron las ratas pegando chillidos\n\
+Me muestran los dientes, cuando las elimino\n\
+Renegado soy y nada puedo hacer\n\
+Siempre vivo y muero en mi propia ley\n\
+En mi propia ley...\n\
+Siempre vivo y muero, en mi propia ley\n\n\
+Surgen las llamas de mi blindado\n\
+mientras avanzo en el mundo eterno\n\
+Renegado soy y nada puedo hacer\n\
+Siempre vivo y muero, en mi propia ley\n\
+En mi propia ley...\n\n\
+Siempre vivo y muero en mi propia ley\n\
+Siempre vivo y muero en mi propia ley\n\
+En mi propia ley..."},
+  { id: 21, title: "SALVAJE CORCEL", lyrics: "Ponen precio a tu cabeza por pensar\n\
+Condenado por errores del ayer\n\
+Convicciones que te hicieron descarriar\n\
+Desenfundaste tus armas otra vez\n\n\
+Aprender de los errores está bien,\n\
+son historias que no debes olvidar\n\
+Viejo eslogan, en tu mente lo sabes\n\
+Mil batallas aun te quedan por pelear\n\n\
+Armado voy, en mi corcel\n\
+Un rayo al viento fui creado para ser\n\
+Aquí estoy, una vez más\n\
+Vamos armados con escudos de metal.\n\n\
+Ponen precio a tu cabeza (por pensar)\n\
+Condenado por errores del ayer\n\
+Convicciones que te hicieron descarriar,\n\
+y desenfundaste tus armas otra vez.\n\n\
+Armado voy, en mi corcel\n\
+Un rayo al viento fui creado para ser\n\
+Aquí estoy, una vez más\n\
+Vamos armados con escudos de metal"},
+  { id: 22, title: "SOS COMO SOS", lyrics: "Quieren destruir el sol\n\
+Quieren invadir el mar\n\
+Deberás tener valor\n\
+Fiel a tu plan ideal\n\
+Vos lo sabes y los vas a enfrentar\n\
+Vos lo sabes y los vas a enfrentar\n\n\
+Forjaste tu protección, con experiencia elemental\n\
+Y frente a la decepción, solo te alejarán\n\n\
+Sos como sos y no, no vas a cambiar\n\
+Sos como sos y no, no vas a cambiar\n\n\
+Quieren destruir el sol\n\
+Quieren invadir el mar\n\
+Deberás tener valor\n\
+Fiel a tu plan ideal\n\n\
+Sos como sos, sos como sos\n\
+Sos como sos y no vas a cambiar"},
+  { id: 23, title: "TRES DESEOS", lyrics: "Los genios se han extinguido\n\
+con sus dagas de poder\n\
+Queda el templo a tu cuidado\n\
+hasta que puedan volver\n\n\
+Que se acabe la tristeza\n\
+rugen todos paz y amor\n\
+Que se acaben las guerras\n\
+ya basta de destrucción\n\n\
+Las visiones traicioneras\n\
+te pueden enceguecer\n\
+distorsionan en tu mente\n\
+y exterminan tu poder\n\n\
+No se si cambies al mundo\n\
+tal vez todo siga igual\n\
+Si es que nada es imposible\n\
+al menos lo intentarás"},
 ];
 
 export const logos = {
