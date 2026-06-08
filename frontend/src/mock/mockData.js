85e41930234396058f0db93a050a13a511cf554a
Federico Sidlik <fedesidlik@gmail.com>
2026-06-08 16:58:24 -0300

    Restore the May 15 show in the listings

--- frontend/src/mock/mockData.js
+++ frontend/src/mock/mockData.js
@@ -43,6 +43,7 @@ export const shows = [
   {
     year: "2026",
     dates: [
+      { date: "15 Mayo 2026", venue: "Gier Music Club" },
       {
         date: "26 Junio 2026",
         time: "23 hs",
